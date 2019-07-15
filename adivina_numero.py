9899c847cc1eadf7368e76d74a7d4e7320ecb8d1
ekco04 <rigelhiram11@gmail.com>
2019-07-15 18:06:43 -0500

    Añadiendo el archivo Adivina_numero

---
+++ adivina_numero.py
@@ -0,0 +1,37 @@
+
+number_to_guess = 4
+
+user_number_first = int(input('Adivina un numero: '))
+
+if number_to_guess == user_number_first:
+    print('¡Has ganado!')
+else:
+    print('Vuelve a intentarlo')
+
+user_number_second = int(input('Adivina un numero: '))
+
+if number_to_guess == user_number_second:
+    print('¡Has ganado!')
+else:
+    print('Vuelve a intentarlo')
+
+user_number_third = int(input('Adivina un numero: '))
+
+if number_to_guess == user_number_third:
+    print('¡Has gando!')
+else:
+    print('Vuelve a intentarlo')
+
+user_number_fourth = int(input('Adivina un numero: '))
+
+if number_to_guess == user_number_fourth:
+    print('¡Has gando!')
+else:
+    print('Vuelve a intentarlo')
+
+user_number_fifth = int(input('Adivina un numero: '))
+
+if number_to_guess == user_number_fifth:
+    print('¡Has ganado!')
+else:
+    print('Has fallado en todos tus intentos')
