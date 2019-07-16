7f5ed61a1a523bad8e3892ead4fa40c2cac11486
ekco04 <rigelhiram11@gmail.com>
2019-07-15 19:28:02 -0500

    Añadiendo el archivo mayor_de_tres

--- mayor_de_tres.py
+++ mayor_de_tres.py
@@ -7,4 +7,5 @@ tercer_numero = int(input('tercer numero: '))
 
 resultado = max(primer_numero, segundo_numero, tercer_numero)
 
-print("El numero mas grande es: {}").format(resultado)
+print("El numero mas grande es: {}".format(resultado))
+
