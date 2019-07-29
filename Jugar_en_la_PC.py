d912dc0b6f7d214535ac58cd53c97c2e61c0b27d
ekco04 <rigelhiram11@gmail.com>
2019-07-29 17:46:39 -0500

    Correcciones programa jugar_en_la_pc

--- Jugar_en_la_PC.py
+++ Jugar_en_la_PC.py
@@ -3,28 +3,52 @@
 tienes_un_pc_input = input('¿Tienes un ordenador? (Si / No): ').upper()
 
 if tienes_un_pc_input == 'SI':
-    tienes_un_pc_input = True
+    tienes_un_pc = True
 elif tienes_un_pc_input == 'NO':
-    tienes_un_pc_input = False
+    tienes_un_pc = False
 else:
-    print('Te he dicho que me digas si o no')
-    tienes_un_pc_input = False
+    print('Te he dicho que me digas si o no, así que lo tomo como que no')
+    tienes_un_pc = False
 
 
 
 tu_pc_grafica_input = input('¿Tu ordenador tiene una gráfica potente? (Si / No): ').upper()
+
+if tu_pc_grafica_input == 'SI':
+    tu_pc_grafica = True
+elif tienes_un_pc_input == 'NO':
+    tu_pc_grafica = False
+else:
+    print('Te he dicho que me digas si o no, así que lo tomo como que no')
+    tienes_un_pc = False
+
+
+
 conexion_internet_cable_input = input('¿Tu ordenador está conectado a internet por cable? (Si / No): ').upper()
+
+if conexion_internet_cable_input == 'SI':
+    conexion_internet_cable = True
+elif conexion_internet_cable_input == 'NO':
+    conexion_internet_cable = False
+else:
+    print('Te he dicho que me digas si o no, así que lo tomo como que no')
+    conexion_internet_cable = False
+
+
+
 conexion_internet_wifi_input = input('¿Tu ordenador está conectado a internet via Wi-Fi? (Si / No): ') .upper()
 
+if conexion_internet_wifi_input == 'SI':
+    conexion_internet_wifi = True
+elif conexion_internet_wifi_input == 'NO':
+    conexion_internet_wifi = False
+else:
+    print('Te he dicho que me digas si o no, así que lo tomo como que no')
+    conexion_internet_wifi = False
 
 
-tienes_un_pc = tienes_un_pc_input == 'SI'
-tu_pc_grafica = tu_pc_grafica_input == 'SI'
-conexion_internet = conexion_internet_cable_input == 'SI' or conexion_internet_wifi_input == 'SI'
 
-
-
-if tienes_un_pc and tu_pc_grafica and conexion_internet:
+if tienes_un_pc and tu_pc_grafica and (conexion_internet_cable or conexion_internet_wifi):
     print('Puedes jugar a todos los juegos que quieras')
 else:
     print('No puedes jugar')
