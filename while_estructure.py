5b08a4eb0b789876d7934687ff48341373b1764e
ekco04 <rigelhiram11@gmail.com>
2019-08-07 18:30:19 -0500

    Añadiendo el archivo while_estructure

---
+++ while_estructure.py
@@ -0,0 +1,11 @@
+numero = 11
+
+while numero > 0:
+    numero -= 1
+    print(numero)
+    if numero % 2 == 0:
+        print('Este numero es par')
+    else:
+        print('Este numero impar')
+
+print('He terminado')
