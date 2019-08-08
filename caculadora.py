4704e76fc3291f426d3d9d30f17d229955ce71f7
ekco04 <rigelhiram11@gmail.com>
2019-08-07 20:05:04 -0500

    Añadiendo el archivo calculadora

---
+++ caculadora.py
@@ -0,0 +1,27 @@
+operación = input('!Hola¡ ¿Qué operacion quieres realizar? (Suma / Resta / Multipilicaión / División):')
+
+while operación == 'Suma':
+    primer_numero = int(input('Dame el primer número: '))
+    segundo_numero = int(input('Dame el segundo número:'))
+    resultado = primer_numero + segundo_numero
+    print('El resultado es {}'.format(resultado))
+
+while operación == 'Resta':
+    primer_numero = int(input('Dame el primer número: '))
+    segundo_numero = int(input('Dame el segundo número:'))
+    resultado = primer_numero - segundo_numero
+    print('El resultado es {}'.format(resultado))
+
+while operación == 'Multipilicaión':
+    primer_numero = input('Dame el primer número: ')
+    segundo_numero = input('Dame el segundo número:')
+    resultado = primer_numero * segundo_numero
+    print('El resultado es {}'.format(resultado))
+
+while operación == 'División':
+    primer_numero = input('Dame el primer número: ')
+    segundo_numero = input('Dame el segundo número:')
+    resultado = primer_numero / segundo_numero
+    print('El resultado es {}'.format(resultado))
+
+print('No me has dicho bien la operacion que quieres que realize')
